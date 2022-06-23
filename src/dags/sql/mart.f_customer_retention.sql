4f103f153ff41aab86606d839612e2312c8871bc
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2022-06-23 08:24:36 +0300

    Update mart.f_customer_retention.sql

--- src/dags/sql/mart.f_customer_retention.sql
+++ src/dags/sql/mart.f_customer_retention.sql
@@ -1,20 +1,23 @@
 INSERT INTO mart.f_customer_retention
 WITH regional_sales AS (
-    SELECT dc.date_actual, fs.customer_id, count(*) retention, sum(fs.payment_amount) payment_amount
+    SELECT dc.date_actual, fs.customer_id, status ,count(*) retention, sum(fs.payment_amount) payment_amount
     FROM mart.f_sales fs 
-    JOIN (SELECT date_id, date_actual FROM mart.d_calendar
-			WHERE --date_actual between '2022-05-01' and '2022-07-01'
-			--and 
-			day_of_week = 1) dc 
+    JOIN (SELECT date_id, date_actual 
+         FROM mart.d_calendar
+			WHERE day_of_week = 1) dc 
 	USING (date_id)
-	GROUP BY dc.date_actual, fs.customer_id
+	GROUP BY dc.date_actual, fs.customer_id, status
    )
-SELECT date_actual,
+SELECT rs.date_actual,
    'weekly' period_name,
-   COUNT(CASE WHEN retention = 1 THEN 1 END) AS new_customers_count,
-   COUNT(CASE WHEN retention > 1 THEN 1 END) AS returning_customers_count,
-   SUM(CASE WHEN retention = 1 THEN payment_amount END) AS new_customers_revenue,
-   SUM(CASE WHEN retention > 1 THEN payment_amount END) AS returning_customers_revenue
-FROM regional_sales
+   COUNT(CASE WHEN rs.retention = 1 THEN 1 END) AS new_customers_count,
+   COUNT(CASE WHEN rs.retention > 1 AND rs.status = 'shipped' THEN 1 END) AS returning_customers_count,
+   COUNT(CASE WHEN rs.status = 'refunded' THEN 1 END) AS refunded_customers_count,
+   SUM(CASE WHEN rs.retention = 1 THEN payment_amount END) AS new_customers_revenue,
+   SUM(CASE WHEN rs.retention > 1 AND rs.status = 'shipped' THEN payment_amount END) AS returning_customers_revenue,
+   SUM(CASE WHEN rs.status = 'refunded' THEN payment_amount else 0 END) AS refunded_customers_revenue
+FROM regional_sales rs
+JOIN mart.d_customer c
+USING (customer_id)
 GROUP BY date_actual
-ON CONFLICT (start_date,period_name) DO UPDATE SET (new_customers_count, returning_customers_count, new_customers_revenue, returning_customers_revenue) = (excluded.new_customers_count, excluded.returning_customers_count, excluded.new_customers_revenue, excluded.returning_customers_revenue);
+ON CONFLICT (start_date,period_name) DO UPDATE SET (new_customers_count, returning_customers_count, refunded_customers_count, new_customers_revenue, returning_customers_revenue, refunded_customers_revenue) = (excluded.new_customers_count, excluded.returning_customers_count, excluded.refunded_customers_count, excluded.new_customers_revenue, excluded.returning_customers_revenue, excluded.refunded_customers_revenue);
