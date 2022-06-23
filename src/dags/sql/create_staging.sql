be59f3973eebf437b7abda72735f0103f79c630c
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2022-06-23 08:23:44 +0300

    Update create_staging.sql

--- src/dags/sql/create_staging.sql
+++ src/dags/sql/create_staging.sql
@@ -9,40 +9,38 @@ drop schema if exists staging cascade;
 create schema if not exists staging;
 
 create table if not exists staging.events_log (
-	id serial NOT NULL,
+	id serial PRIMARY KEY,
   	datetime timestamp NOT NULL DEFAULT NOW(),
-  	proc_name varchar(100) NOT NULL,
+  	proc_name varchar(100) NOT NULL DEFAULT 'Upload',
   	target_table varchar(100),
   	source_file varchar(1000),
-  	duration_ms bigint,
+  	duration_ms int,
 	rows bigint,
-  	status varchar(100) NOT NULL,
+  	status varchar(100) NOT NULL DEFAULT 'Success',
   	msg varchar(1000), 
-  	target_date date,
-  	CONSTRAINT events_log_pkey PRIMARY KEY (id)
+  	target_date date
 );
 
 create table if not exists staging.customer_research (
-	id serial NOT NULL,
+	id serial PRIMARY KEY,
 	date_id timestamp NOT NULL,
 	category_id int NOT NULL,
 	geo_id int NOT NULL,
 	sales_qty int,
-	sales_amt numeric(14,2),
-	CONSTRAINT customer_research_pkey PRIMARY KEY (id)
+	sales_amt numeric(14,2)
 );
 
 create table if not exists staging.user_activity_log (
-	id serial NOT NULL,
+	id serial PRIMARY KEY,
 	date_time timestamp NOT NULL,
 	action_id bigint NOT NULL,
 	customer_id bigint NOT NULL,
-	quantity bigint,
-	CONSTRAINT user_activity_log_pkey PRIMARY KEY (id)
+	quantity int8
 );
+--CREATE INDEX ual1 ON staging.user_activity_log USING btree (customer_id);
 
 create table if not exists staging.user_order_log (
-	id serial NOT NULL,
+	id serial PRIMARY KEY,
 	date_time timestamp NOT NULL,
 	city_id int4 NOT NULL,
 	city_name varchar(100) NULL,
@@ -52,19 +50,19 @@ create table if not exists staging.user_order_log (
 	item_id int4 NOT NULL,
 	item_name varchar(100) NULL,
 	quantity int8 NULL,
-	payment_amount numeric(10, 2) NULL,
-	CONSTRAINT user_order_log_pkey PRIMARY KEY (id)
+	payment_amount numeric(10, 2) NULL
 );
-CREATE INDEX uo1 ON staging.user_order_log USING btree (customer_id);
-CREATE INDEX uo2 ON staging.user_order_log USING btree (item_id);
+--CREATE INDEX uo1 ON staging.user_order_log USING btree (customer_id);
+--CREATE INDEX uo2 ON staging.user_order_log USING btree (item_id);
 
 create table if not exists staging.price_log (
-	id serial NOT NULL,
+	id serial PRIMARY KEY,
   	datetime timestamp NOT NULL,
   	category_id int NOT NULL,
   	category_name varchar(100) NOT NULL,
   	item_id int NOT NULL,
   	price int,
-  	batch_id int,
-  	CONSTRAINT price_log_pkey PRIMARY KEY (id)
+  	batch_id int
 );
+--CREATE INDEX pl1 ON staging.price_log USING btree (category_id);
+--CREATE INDEX pl2 ON staging.price_log USING btree (item_id);
